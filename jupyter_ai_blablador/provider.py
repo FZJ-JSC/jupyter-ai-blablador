37d1d6ed47ec4a9385213d11ec841ad0bafb8178
goebbert1 <goebbert1@jwlogin10.juwels>
2024-08-03 21:28:40 +0200

    add chat- and embedding-model

--- jupyter_ai_blablador/provider.py
+++ jupyter_ai_blablador/provider.py
@@ -1,75 +1,79 @@
 from typing import ClassVar, List
 
-from langchain_openai import ChatOpenAI, OpenAI
+from langchain_openai import ChatOpenAI, OpenAI, OpenAIEmbeddings
 
-from jupyter_ai import AuthStrategy, EnvAuthStrategy, BaseProvider, Field
-
+from jupyter_ai import AuthStrategy, EnvAuthStrategy, Field
+from jupyter_ai_magics import BaseProvider, BaseEmbeddingsProvider
 
 class BlabladorProvider(BaseProvider, OpenAI):
-    """
-    A test model provider implementation for developers to build from. A model
-    provider inherits from 2 classes: 1) the `BaseProvider` class from
-    `jupyter_ai`, and 2) an LLM class from `langchain`, i.e. a class inheriting
-    from `LLM` or `BaseChatModel`.
-
-    Any custom model first requires a `langchain` LLM class implementation.
-    Please import one from `langchain`, or refer to the `langchain` docs for
-    instructions on how to write your own. We offer an example in `./llm.py` for
-    testing.
-
-    To create a custom model provider from an existing `langchain`
-    implementation, developers should edit this class' declaration to
-
-    ```
-    class TestModelProvider(BaseProvider, <langchain-llm-class>):
-        ...
-    ```
-
-    Developers should fill in each of the below required class attributes.
-    As the implementation is provided by the inherited LLM class, developers
-    generally don't need to implement any methods. See the built-in
-    implementations in `jupyter_ai_magics.providers.py` for further reference.
-
-    The provider is made available to Jupyter AI by the entry point declared in
-    `pyproject.toml`. If this class or parent module is renamed, make sure the
-    update the entry point there as well.
-    """
-
     id: ClassVar[str] = "blablador"
-    """ID for this provider class."""
-
     name: ClassVar[str] = "Blablador"
-    """User-facing name of this provider."""
-
     models: ClassVar[List[str]] = [
         "alias-code",
-        "alias-embeddings",
         "alias-fast",
         "alias-large",
     ]
-    """List of supported models by their IDs. For registry providers, this will
-    be just ["*"]."""
-
-    help: ClassVar[str] = None
-    """Text to display in lieu of a model list for a registry provider that does
-    not provide a list of models."""
-
+    # help: ClassVar[str] = None
     model_id_key: ClassVar[str] = "model_name"
-    """Kwarg expected by the upstream LangChain provider."""
-
     model_id_label: ClassVar[str] = "Model ID"
-    """Human-readable label of the model ID."""
-
     pypi_package_deps: ClassVar[List[str]] = ["langchain_openai"]
-    """List of PyPi package dependencies."""
-
     auth_strategy: ClassVar[AuthStrategy] = EnvAuthStrategy(name="OPENAI_API_KEY")
-    """Authentication/authorization strategy. Declares what credentials are
-    required to use this model provider. Generally should not be `None`."""
-
-    registry: ClassVar[bool] = False
-    """Whether this provider is a registry provider."""
-
+    # registry: ClassVar[bool] = False
     openai_api_base: ClassVar[str] = "https://helmholtz-blablador.fz-juelich.de:8000/v1" 
     # openai_api_key: 
     # openai_organization:
+
+    @classmethod
+    def is_api_key_exc(cls, e: Exception):
+        """
+        Determine if the exception is an OpenAI API key error.
+        """
+        import openai
+
+        if isinstance(e, openai.AuthenticationError):
+            error_details = e.json_body.get("error", {})
+            return error_details.get("code") == "invalid_api_key"
+        return False
+
+
+class ChatBlabladorProvider(BaseProvider, ChatOpenAI):
+    id: ClassVar[str] = "blablador-chat"
+    name: ClassVar[str] = "Blablador"
+    models: ClassVar[List[str]] = [
+        "alias-code",
+        "alias-fast",
+        "alias-large",
+    ]
+    # help: ClassVar[str] = None
+    model_id_key: ClassVar[str] = "model_name"
+    model_id_label: ClassVar[str] = "Model ID"
+    pypi_package_deps: ClassVar[List[str]] = ["langchain_openai"]
+    auth_strategy: ClassVar[AuthStrategy] = EnvAuthStrategy(name="OPENAI_API_KEY")
+    # registry: ClassVar[bool] = False
+    openai_api_base: ClassVar[str] = "https://helmholtz-blablador.fz-juelich.de:8000/v1"
+    # openai_api_key:
+    # openai_organization:
+
+    @classmethod
+    def is_api_key_exc(cls, e: Exception):
+        """
+        Determine if the exception is an OpenAI API key error.
+        """
+        import openai
+
+        if isinstance(e, openai.AuthenticationError):
+            error_details = e.json_body.get("error", {})
+            return error_details.get("code") == "invalid_api_key"
+        return False
+
+
+class BlabladorEmbeddingsProvider(BaseEmbeddingsProvider, OpenAIEmbeddings):
+    id: ClassVar[str] = "blablador"
+    name: ClassVar[str] = "Blablador"
+    models: ClassVar[List[str]] = [
+        "alias-embeddings",
+    ]
+    model_id_key = "model"
+    pypi_package_deps = ["langchain_openai"]
+    auth_strategy = EnvAuthStrategy(name="OPENAI_API_KEY")
+    openai_api_base: ClassVar[str] = "https://helmholtz-blablador.fz-juelich.de:8000/v1"
