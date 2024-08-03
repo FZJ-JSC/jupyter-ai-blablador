263dccf795812c8a8b40ca617ce8b3eab17d1729
goebbert1 <goebbert1@jwlogin10.juwels>
2024-08-03 16:47:51 +0200

    step 1

--- jupyter_ai_blablador/provider.py
+++ jupyter_ai_blablador/provider.py
@@ -1,11 +1,11 @@
 from typing import ClassVar, List
 
+from langchain_openai import ChatOpenAI, OpenAI
+
 from jupyter_ai import AuthStrategy, BaseProvider, Field
 
-from .llm import TestLLM
 
-
-class TestProvider(BaseProvider, TestLLM):
+class BlabladorProvider(BaseProvider, OpenAI):
     """
     A test model provider implementation for developers to build from. A model
     provider inherits from 2 classes: 1) the `BaseProvider` class from
@@ -35,13 +35,18 @@ class TestProvider(BaseProvider, TestLLM):
     update the entry point there as well.
     """
 
-    id: ClassVar[str] = "test-provider"
+    id: ClassVar[str] = "blablador"
     """ID for this provider class."""
 
-    name: ClassVar[str] = "Test Provider"
+    name: ClassVar[str] = "Blablador"
     """User-facing name of this provider."""
 
-    models: ClassVar[List[str]] = ["test-model-1"]
+    models: ClassVar[List[str]] = [
+        "alias-code",
+        "alias-embeddings",
+        "alias-fast",
+        "alias-large",
+    ]
     """List of supported models by their IDs. For registry providers, this will
     be just ["*"]."""
 
@@ -49,22 +54,30 @@ class TestProvider(BaseProvider, TestLLM):
     """Text to display in lieu of a model list for a registry provider that does
     not provide a list of models."""
 
-    model_id_key: ClassVar[str] = "model_id"
+    model_id_key: ClassVar[str] = "model_name"
     """Kwarg expected by the upstream LangChain provider."""
 
     model_id_label: ClassVar[str] = "Model ID"
     """Human-readable label of the model ID."""
 
-    pypi_package_deps: ClassVar[List[str]] = []
+    pypi_package_deps: ClassVar[List[str]] = ["langchain_openai"]
     """List of PyPi package dependencies."""
 
-    auth_strategy: ClassVar[AuthStrategy] = None
+    auth_strategy: ClassVar[AuthStrategy] = EnvAuthStrategy(name="OPENAI_API_KEY")
     """Authentication/authorization strategy. Declares what credentials are
     required to use this model provider. Generally should not be `None`."""
 
     registry: ClassVar[bool] = False
     """Whether this provider is a registry provider."""
 
-    fields: ClassVar[List[Field]] = []
+    fields: ClassVar[List[Field]] = [
+        TextField(
+            key="openai_api_base", label="Base API URL (optional)", format="text"
+        ),
+        TextField(
+            key="openai_organization", label="Organization (optional)", format="text"
+        ),
+        TextField(key="openai_proxy", label="Proxy (optional)", format="text"),
+    ]
     """User inputs expected by this provider when initializing it. Each `Field` `f`
-    should be passed in the constructor as a keyword argument, keyed by `f.key`."""
+    should be passed in the constructor as a keyword argument, keyed by `f.key`.""i"
