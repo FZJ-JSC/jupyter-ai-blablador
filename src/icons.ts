3b1e6c561ea72906797a9a9b87a6a59eb91a4a80
goebbert1 <goebbert1@jwlogin10.juwels>
2024-08-04 10:07:15 +0200

    prepare for blablador icon

---
+++ src/icons.ts
@@ -0,0 +1,10 @@
+// This file is based on iconimports.ts in @jupyterlab/ui-components, but is manually generated.
+
+import { LabIcon } from '@jupyterlab/ui-components';
+
+import blabladorSvg from '../style/icons/blablador.svg';
+
+export const blabladorIcon = new LabIcon({
+  name: 'jupyter-ai-blablador::blablador',
+  svgstr: blabladorSvg
+});
